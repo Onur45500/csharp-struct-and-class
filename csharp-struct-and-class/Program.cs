375f32e033412a13b1e5dccc797c563dd1e89c40
Onur45500 <onur.akmese@viacesi.fr>
2024-09-19 10:00:45 +0200

    Made different contructor in the class, the name of the constructor is the same but the number of parameters and their types change

--- csharp-struct-and-class/Program.cs
+++ csharp-struct-and-class/Program.cs
@@ -2,6 +2,7 @@
 {
     public class Program
     {
+        // The constructor of a struct can't be empty.
         //struct Person
         //{
         //    public string name;
@@ -16,17 +17,38 @@
         //    }
         //}
 
+        // The difference with struct is the fact that the constructor can be empty and we can have multiple constructor
         class Person
         {
             public string name;
             public string surname;
             public string identifier;
+            public int age;
 
+            // The number of arguments and their types allow us to differentiat between the constructor, the name here don't matter
             public Person(string name, string surname, string identifier)
             {
                 this.name = name;
                 this.surname = surname;
                 this.identifier = identifier;
+            }
+
+            public Person(string name, string surname)
+            {
+                this.name = name;
+                this.surname = surname;
+            }
+
+            public Person(string name)
+            {
+                this.name = name;
+                this.surname = "O";
+            }
+
+            public Person(int age)
+            {
+                this.age = age;
+                this.surname = "O";
             }
         }
 
