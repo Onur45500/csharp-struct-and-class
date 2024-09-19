d2c6a35aa39ca60c7ba922db4c7f35f0280a62a8
Onur45500 <onur.akmese@viacesi.fr>
2024-09-19 09:54:36 +0200

    Added a simple class to see the difference with struct

--- csharp-struct-and-class/Program.cs
+++ csharp-struct-and-class/Program.cs
@@ -2,8 +2,21 @@
 {
     public class Program
     {
+        //struct Person
+        //{
+        //    public string name;
+        //    public string surname;
+        //    public string identifier;
 
-        struct Person
+        //    public Person(string name, string surname, string identifier)
+        //    {
+        //        this.name = name;
+        //        this.surname = surname;
+        //        this.identifier = identifier;
+        //    }
+        //}
+
+        class Person
         {
             public string name;
             public string surname;
@@ -19,14 +32,17 @@
 
         static void Main(string[] args)
         {
-            Person person;
-            person.name = "Onur";
-            person.surname = "AKMESE";
-            person.identifier = "AOFDODODOD";
-            Console.WriteLine($"{person.name} - {person.surname} - {person.identifier}");
+            //Person person;
+            //person.name = "Onur";
+            //person.surname = "AKMESE";
+            //person.identifier = "AOFDODODOD";
+            //Console.WriteLine($"{person.name} - {person.surname} - {person.identifier}");
 
-            Person person2 = new Person("Onur", "AKMESE", "AOFDODODOD");
-            Console.WriteLine($"{person2.name} - {person2.surname} - {person2.identifier}");
+            //Person person2 = new Person("Onur", "AKMESE", "AOFDODODOD");
+            //Console.WriteLine($"{person2.name} - {person2.surname} - {person2.identifier}");
+
+            Person person = new Person("Onur", "AKMESE", "AOFDODODOD");
+
         }
     }
 }
