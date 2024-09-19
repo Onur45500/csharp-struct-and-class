4d0a07d9b5299716fa8dd3526cbe9839b3308663
Onur45500 <onur.akmese@viacesi.fr>
2024-09-19 09:45:53 +0200

    Created a simple struct for person

--- csharp-struct-and-class/Program.cs
+++ csharp-struct-and-class/Program.cs
@@ -2,9 +2,22 @@
 {
     public class Program
     {
+
+        struct Person
+        {
+            public string name;
+            public string surname;
+            public string identifier;
+        }
+
         static void Main(string[] args)
         {
+            Person Onur;
+            Onur.name = "Onur";
+            Onur.surname = "AKMESE";
+            Onur.identifier = "AOFDODODOD";
 
+            Console.WriteLine($"{Onur.name} - {Onur.surname} - {Onur.identifier}");
         }
     }
 }
